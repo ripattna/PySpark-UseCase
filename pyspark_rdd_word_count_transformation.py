27ea1bffd5644a2762043651a72acf886b7607e3
ripattna <ripattna@gmail.com>
2021-12-03 12:51:14 +0530

    Code Change

--- pyspark_rdd_word_count_transformation.py
+++ pyspark_rdd_word_count_transformation.py
@@ -1,18 +1,18 @@
 """
-This is Rdd Transformation Example
+This is Word Count program
+Instead of using flatMap we are using map
 """
-from pyspark import SparkContext
+from pyspark import SparkContext, SparkConf
 
 if __name__ == "__main__":
 
     # Create Spark context with necessary configuration
-    # conf = SparkConf().setAppName("appName").setMaster("local")
-    # sc = SparkContext(conf=conf)
+    conf = SparkConf().setAppName("appName").setMaster("local")
+    sc = SparkContext(conf=conf)
 
-    sc = SparkContext("local", "PySpark")
-
-    readRDD = sc.textFile("resources/Input.txt")
-
+    # Read the file
+    readRDD = sc.textFile("resources/WordCount_Sample.txt")
+    # Applying filter transformation
     nonempty_lines = readRDD.filter(lambda x: len(x) > 0)
 
     # print(nonempty_lines.collect())
@@ -25,3 +25,5 @@ if __name__ == "__main__":
         .sortByKey(False)
 
     print(wordcount.collect())
+    print("The word count of the file is:", wordcount.count())
+
