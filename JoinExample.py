52a686c729261fae8d50d6256678498905c24a92
ripattna <ripattna@gmail.com>
2022-02-18 21:36:52 +0530

    Code Change

--- JoinExample.py
+++ JoinExample.py
@@ -2,15 +2,15 @@ import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 
-spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+spark = SparkSession.builder.appName('JoinExample.com').getOrCreate()
 
 emp = [(1, "Smith", -1, "2018", "10", "M", 3000),
        (2, "Rose", 1, "2010", "20", "M", 4000),
        (3, "Williams", 1, "2010", "10", "M", 1000),
        (4, "Jones", 2, "2005", "10", "F", 2000),
        (5, "Brown", 2, "2010", "40", "", -1),
-       (6, "Brown", 2, "2010", "50", "", -1)
-       ]
+       (6, "Brown", 2, "2010", "50", "", -1)]
+
 empColumns = ["emp_id", "name", "superior_emp_id", "year_joined",
               "emp_dept_id", "gender", "salary"]
 
@@ -21,8 +21,8 @@ empDF.show(truncate=False)
 dept = [("Finance", 10),
         ("Marketing", 20),
         ("Sales", 30),
-        ("IT", 40)
-        ]
+        ("IT", 40)]
+
 deptColumns = ["dept_name", "dept_id"]
 deptDF = spark.createDataFrame(data=dept, schema=deptColumns)
 deptDF.printSchema()
@@ -64,8 +64,6 @@ empDF.alias("emp1").join(empDF.alias("emp2"),
 empDF.createOrReplaceTempView("EMP")
 deptDF.createOrReplaceTempView("DEPT")
 
-joinDF = spark.sql("select * from EMP e, DEPT d where e.emp_dept_id == d.dept_id") \
-    .show(truncate=False)
+spark.sql("select * from EMP e, DEPT d where e.emp_dept_id == d.dept_id").show(truncate=False)
 
-joinDF2 = spark.sql("select * from EMP e INNER JOIN DEPT d ON e.emp_dept_id == d.dept_id") \
-    .show(truncate=False)
+spark.sql("select * from EMP e INNER JOIN DEPT d ON e.emp_dept_id == d.dept_id").show(truncate=False)
